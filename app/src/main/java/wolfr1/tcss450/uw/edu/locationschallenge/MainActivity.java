62a13f0e3107e4c8e4cba332af1e9e722ffeb89f
ruitaoyu <ruitaoyu@uw.edu>
2019-02-27 15:23:33 -0800

    cycling color marker

--- app/src/main/java/wolfr1/tcss450/uw/edu/locationschallenge/MainActivity.java
+++ app/src/main/java/wolfr1/tcss450/uw/edu/locationschallenge/MainActivity.java
@@ -3,6 +3,7 @@ package wolfr1.tcss450.uw.edu.locationschallenge;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -25,6 +26,8 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -32,8 +35,10 @@ import com.google.android.gms.tasks.OnSuccessListener;
 
 import java.util.ArrayList;
 
+
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback,
-        GoogleMap.OnMapClickListener {
+        GoogleMap.OnMapClickListener,
+        GoogleMap.OnCameraMoveStartedListener {
 
     private static final String TAG = "MyLocationsActivity";
     /**
@@ -54,18 +59,37 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     private FusedLocationProviderClient mFusedLocationClient;
     private LocationCallback mLocationCallback;
     private GoogleMap mMap;
+    private FloatingActionButton fab;
+    private boolean mIsFollowing;
+    private Marker mCurrentMarker;
 
     // challange
     ArrayList<MarkerOptions> mMarkerArrayList = new ArrayList<>();
+   // ArrayList<Marker> m
+
+    //Color[] colors = new Color[7];
+
+    float[] colors = {BitmapDescriptorFactory.HUE_BLUE,BitmapDescriptorFactory.HUE_GREEN,
+            BitmapDescriptorFactory.HUE_AZURE,BitmapDescriptorFactory.HUE_MAGENTA,
+            BitmapDescriptorFactory.HUE_ROSE,BitmapDescriptorFactory.HUE_YELLOW};
+
+    int mColorIndex = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        mCurrentLocation = new Location("");
+
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
+
+        mIsFollowing = true;
+
+        fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -75,23 +99,16 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                             .setAction("Action", null).show();
 
                 } else {
-                    //Do nothing
-//                    Intent i = new Intent(MyLocationsActivity.this, MapsActivity.class);
-//                    //pass the current location on to the MapActivity when it is loaded
-//                    i.putExtra("LOCATION", mCurrentLocation);
-//                    startActivity(i);
+                    // SNAP TO MARKER LOCATION
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng (mCurrentLocation.getLatitude(), mCurrentLocation.getLongitude()), 18.0f));
+                    fab.setEnabled(false);
+                    mIsFollowing = true;
                 }
             }
         });
 
-        setContentView(R.layout.activity_main);
 
-        mCurrentLocation = (Location) getIntent().getParcelableExtra("LOCATION");
 
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
 
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
 
@@ -106,7 +123,10 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                     MY_PERMISSIONS_LOCATIONS);
         } else {
             //The user has already allowed the use of Locations. Get the current location.
+            Log.e("Location 0 ============= ", "" + mCurrentLocation);
             requestLocation();
+            Log.e("Location 1 ============= ", "" + mCurrentLocation);
+
         }
 
         mLocationCallback = new LocationCallback() {
@@ -121,15 +141,37 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                     // ...
                     setLocation(location);
                     Log.d("LOCATION UPDATE!", location.toString());
+                    LatLng currentLatLong = new LatLng(mCurrentLocation.getLatitude(), mCurrentLocation.getLongitude());
+                    mCurrentMarker.setPosition(currentLatLong);
+                    if (mIsFollowing){
+                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLatLong, 18.0f));
+                    }
                 }
             };
         };
 
+//        Log.e("Location 1 ============= ", "" + mCurrentLocation);
         createLocationRequest();
+//        Log.e("Location 2 ============= ", "" + mCurrentLocation);
+
+
+        setContentView(R.layout.activity_maps);
+
+//        mCurrentLocation = (Location) getIntent().getParcelableExtra("LOCATION");
+
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
     }
 
+
     private void setLocation(final Location location) {
+//        Log.e("Location 0.0 ============= ", "" + mCurrentLocation);
         mCurrentLocation = location;
+//        Log.e("Location 0.1 ============= ", "" + mCurrentLocation);
     }
 
     @Override
@@ -197,7 +239,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     }
 
     private void requestLocation() {
-
+        Log.e("Location Requested !!!!!!!!!!!!!!!!!!!!!!!!!!!!", "");
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED
                 && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
@@ -206,11 +248,12 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             Log.d("REQUEST LOCATION", "User did NOT allow permission to request location!");
 
         } else {
-
+//            Log.e("Request Location 1 ============= ", "");
             mFusedLocationClient.getLastLocation()
                     .addOnSuccessListener(this, new OnSuccessListener<Location>() {
                         @Override
                         public void onSuccess(Location location) {
+                            Log.e("Request Location Success ============= ", "");
                             // Got last known location. In some rare situations this can be null.
                             if (location != null) {
                                 setLocation(location);
@@ -267,7 +310,8 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
         // Add a marker in the current device location and move the camera
         LatLng current = new LatLng(mCurrentLocation.getLatitude(), mCurrentLocation.getLongitude());
-        mMap.addMarker(new MarkerOptions().position(current).title("Current Location"));
+        mCurrentMarker = mMap.addMarker(new MarkerOptions().position(current).title("Current Location"));
+
         //Zoom levels are from 2.0f (zoomed out) to 21.f (zoomed in)
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(current, 15.0f));
         mMap.setOnMapClickListener(this);
@@ -286,11 +330,23 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         // then add the current marker
         MarkerOptions mk = new MarkerOptions().position(latLng)
                 .title("New Marker " + index);
+        if(mColorIndex >=6) {
+            mColorIndex = 0;
+        } else {
+            mColorIndex++;
+        }
+        mk.icon(BitmapDescriptorFactory.defaultMarker(colors[mColorIndex]));
         Marker marker = mMap.addMarker(mk);
         marker.setTag(index);
         mMarkerArrayList.add(mk);
 
         //mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15.0f));
+    }
+
+    @Override
+    public void onCameraMoveStarted(int i) {
+        fab.setEnabled(true);
+        mIsFollowing = false;
     }
 
 }
